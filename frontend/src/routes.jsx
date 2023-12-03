88d17ae5578b872319f21260eb40f62bf729caeb
JuniYadi <juniyadi94@gmail.com>
2023-12-03 12:11:58 +0700

    Update Routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -69,6 +69,10 @@ export const router = createBrowserRouter([
         path: "",
         element: <NotesCreate />,
       },
+      {
+        path: "lists",
+        element: <Notes />,
+      },
     ],
   },
   {
